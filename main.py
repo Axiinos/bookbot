0bbc15ecab5de8467c4eec3d4479377a94c88d0e
Axiinos <syrusbrian@gmail.com>
2024-10-14 13:27:02 +0200

    Added new function, a infinite loop with a n escape, along with error checking if the user input correct path.

--- main.py
+++ main.py
@@ -1,26 +1,49 @@
+import os
+
 def main():
 
-    # Get path to book
-    book_path = str(input())
+    while True:
     
-    # Print the start of output
-    print(f"--- Analyzing the book {book_path} --- \n")
+        # Get path to book
+        try:
+            book_path = str(input("Enter the path to the book: "))
+            if not book_path:
+                raise FileNotFoundError
+            
+            allowed_extensions = ['.txt', '.pdf', '.docx', '.doc', '.epub', '']
+            _, ext = os.path.splitext(book_path)
+            if ext not in allowed_extensions:
+                print("Invalid file extension! Try again")
+                continue
+            
+        except (FileNotFoundError, IsADirectoryError):
+            print("Invalid path! Try again")
+            continue
+        # Print the start of output
+        print(f"--- Analyzing the book {book_path} --- \n")
+            
+            
+        # Get the text from the book and count the words
+        text = get_book_text(book_path)
+        if not text:
+            print("Are you sure you entered the correct path to the book? Try again")
+            continue
+        word_c = word_count(text)
+        print(f"Word Count:\n{word_c} number of words found in the book\n")
         
+        # Count characters
+        char_counts = character_count(text)
         
-    # Get the text from the book and count the words
-    text = get_bok_text(book_path)
-    word_c = word_count(text)
-    print(f"Word Count:\n{word_c} number of words found in the book\n")
-    
-    # Count characters
-    char_counts = character_count(text)
-    
-    # Sort the characters by their count
-    print(f"Character Count: \n")
-    sorting(char_counts)
+        # Sort the characters by their count
+        print(f"Character Count: \n")
+        sorting(char_counts)
 
-    print(f"--- End of analysis ---")
-    
+        print(f"--- End of analysis ---\n\n")
+        
+        if str(input("Do you want to analyze another book? press Enter for yes or type in n for no : ")) == "n":
+            break 
+            
+            
 def sorting(char_counts):
     
     # Sort the characters by their count
@@ -50,10 +73,15 @@ def word_count(text):
     words = text.split()
     return len(words)
     
-def get_bok_text(path):
+def get_book_text(path):
     
     # Get the text from the book
-    with open(path, "r") as file:
-        return file.read()
+    try:
+        with open(path, "r") as file:
+            return file.read()
+    except Exception as e:
+        print(f"Error reading the book: {e}")
+        return None
+    
     
 main()
